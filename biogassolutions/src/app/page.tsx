325231c3f809e4927d3ef511e052d67641951ce8
Cuby92 <jakub.bartul@hotmail.com>
2026-03-13 17:30:53 +0100

    Copy images used on homepage and update paths

--- biogassolutions/src/app/page.tsx
+++ biogassolutions/src/app/page.tsx
@@ -16,38 +16,38 @@ export default function Home() {
           <div id="Section2">
               <h2>Co robimy?</h2>
               <div className="column card">
-                  <img src="./images/biogas.png" alt="Ikona biogazowni" />
+                  <img src="/img/icons/biogas.png" alt="Ikona biogazowni" />
                   <h3>Mikrobiogazownie do 50 kW</h3>
               </div>
               <div className="column card">
-                  <img src="./images/power-plant.png" alt="Ikona dużej biogazowni" />
+                  <img src="/img/icons/power-plant.png" alt="Ikona dużej biogazowni" />
                   <h3>Biogazownie 50-499 kW i {'>'}500 kW</h3> 
               </div>
               <div className="column card">
-                  <img src="./images/methane.png" alt="Ikona metanu" />
+                  <img src="/img/icons/methane.png" alt="Ikona metanu" />
                   <h3>Biometanownie</h3>  
               </div>
               <div className="cta">
-              <Link href="/oferta" className="cta-button">Poznaj naszą ofertę</Link>
+                <Link href="/oferta" className="cta-button">Poznaj naszą ofertę</Link>
               </div>
           </div>
 
           <div id="Section3">
               <h2>Dlaczego my?</h2>
               <div className="card">
-                  <img src="./images/workflow.png" alt="Ikona przepływu pracy" />
+                  <img src="/img/icons/workflow.png" alt="Ikona przepływu pracy" />
                   <p>Kompleksowa obsługa „od A do Z” (studium, projekt, decyzje, budowa, rozruch, serwis) i wsparcie pozyskania dotacji.</p>
               </div>
               <div className="card">
-                  <img src="./images/technology.png" alt="Ikona technologii" />
+                  <img src="/img/icons/technology.png" alt="Ikona technologii" />
                   <p>Zaawansowane systemy procesowe - w zakresie urządzeń i elementów procesowych (w tym procesów higienizacji oraz sterylizacji), zdalnego sterowania oraz instalacji dla funkcjonalności obiektów.</p>
               </div>
               <div className="card">
-                  <img src="./images/24-hours-service.png" alt="Ikona obsługa 24/7" />
+                  <img src="/img/icons/24-hours-service.png" alt="Ikona obsługa 24/7" />
                   <p>Zdalny monitoring 24/7, szkolenia operatorów i standard reakcji serwisu do 48 h z opcją skrócenia wg umowy.</p>
               </div>
               <div className="card">
-                  <img src="./images/shield.png" alt="Ikona tarczy" />
+                  <img src="/img/icons/shield.png" alt="Ikona tarczy" />
                   <p>Bezpieczeństwo i niezawodność instalacji w zakresie całości obiektów oraz poszczególnych urządzeń i elementów.</p>
               </div>
               <div className="cta">
@@ -57,18 +57,18 @@ export default function Home() {
 
           <div id="Section4">
               <h2>Proces inwestycyjny</h2>
-              <img className="image-animation" src="./images/timeline.png" alt="Przebieg procesu inwestycyjnego - oś czasu" />
+              <img src="/img/timeline.png" alt="Przebieg procesu inwestycyjnego - oś czasu" />
               <Link href="/technologia" className="cta-button">Poznaj technologię</Link>
           </div>
 
           <div id="Section5">
               <h2>Finansowanie</h2>
-              <p className="fade-out">Przygotowujemy dokumentację i wnioski oraz wspieramy rozliczenia w programach krajowych i unijnych.</p>
+              <p>Przygotowujemy dokumentację i wnioski oraz wspieramy rozliczenia w programach krajowych i unijnych.</p>
 
               <div className="logos">
-                  <img className="image-animation" src="./images/FENG_logo.png" alt="Fundusze Europejskie dla Nowoczesnej Gospodarki logo"/>
-                  <img className="image-animation" src="./images/NFOSiGW_logo.png" alt="Narodowy Fundusz Ochrony Środowiska i Gospodarki Wodnej logo" />
-                  <img className="image-animation" src="./images/energia-dla-wsi.png" alt="Energia Dla Wsi logo" />
+                  <img src="/img/dotations/feng-logo.png" alt="Fundusze Europejskie dla Nowoczesnej Gospodarki"/>
+                  <img src="/img/dotations/nfosigw-logo.png" alt="Narodowy Fundusz Ochrony Środowiska i Gospodarki Wodnej" />
+                  <img src="/img/dotations/energia-dla-wsi-logo.png" alt="Energia Dla Wsi" />
               </div>
 
               <div className="cta">
