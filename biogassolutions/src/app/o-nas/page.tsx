04204c091013aec16540f1ba326d45ddf7a7c3c3
Cuby92 <jakub.bartul@hotmail.com>
2026-03-13 18:05:45 +0100

    Add images to about page

--- biogassolutions/src/app/o-nas/page.tsx
+++ biogassolutions/src/app/o-nas/page.tsx
@@ -1,4 +1,5 @@
 import Link from 'next/link';
+import './page.css';
 
 function About() {
     return (
@@ -14,11 +15,11 @@ function About() {
                     <div id="Section2">
                         <h2>Jak pracujemy?</h2>
                         <div className="card">
-                            <img src="./images/hand.png" alt="Ikona finansowania" />
+                            <img src="/img/icons/hand.png" alt="Ikona finansowania" />
                             <p>Wspieramy pozyskanie dofinansowania, przygotowując dokumentację i rozliczenia.</p>
                         </div>
                         <div className="card">
-                            <img src="./images/workflow.png" alt="Ikona przepływu pracy" />
+                            <img src="/img/icons/workflow.png" alt="Ikona przepływu pracy" />
                             <p>Kompleksowo prowadzimy inwestycję: studium wykonalności, projekt, decyzje, budowa, rozruch i wsparcie eksploatacyjne.</p>
                         </div>
                         <div className="cta"><Link className="cta-button" href="/kontakt">Uzyskaj bezpłatną wstępną wycenę</Link></div>
@@ -28,17 +29,17 @@ function About() {
                         <h2>Technologia i dobór</h2>
 
                         <div className="card">
-                            <img src="./images/settings.png" alt="Ikona kół zębatych" />
+                            <img src="/img/icons/settings.png" alt="Ikona kół zębatych" />
                             <p>Konfiguracje dobieramy do dostępnych substratów, sezonowości i profilu zapotrzebowania na energię.</p>
                         </div>
 
                         <div className="card">
-                            <img src="./images/elasticity.png" alt="Ikona elastyczności" />
+                            <img src="/img/icons/elasticity.png" alt="Ikona elastyczności" />
                             <p>Zapewniamy elastyczność instalacji dzięki zastosowaniu buforów technologicznych, co pozwala dostosować się do zmienności rynku substratów i utrzymać wysoką niezawodność na lata.</p>
                         </div>
 
                         <div className="card">
-                            <img src="./images/industry-tank.png" alt="Ikona zbiornika" />
+                            <img src="/img/icons/industry-tank.png" alt="Ikona zbiornika" />
                             <p>Stawiamy na materiały najwyższej jakości.</p>
                         </div>
                         <div className="cta"><Link className="cta-button" href="/technologia">Poznaj technologię</Link></div>
@@ -48,12 +49,12 @@ function About() {
                         <h2>Gdzie działamy?</h2>
 
                         <div className="card">
-                            <img src="./images/planet-earth.png" alt="Ikona Ziemi" />
+                            <img src="/img/icons/planet-earth.png" alt="Ikona Ziemi" />
                             <p>Działamy w całej Polsce oraz w krajach europejskich; oferujemy konsultacje na miejscu.</p>
                         </div>
 
                         <div className="card">
-                            <img src="./images/graduate-hat.png" alt="Ikona czapki absolwenta" id="graduate-hat-icon" />
+                            <img src="/img/icons/graduate-hat.png" alt="Ikona czapki absolwenta" id="graduate-hat-icon" />
                             <p>Zapewniamy szkolenia, zdalny monitoring 24/7 i serwis zgodnie z zapisami umowy serwisowej (SLA).</p>
                         </div>
 
