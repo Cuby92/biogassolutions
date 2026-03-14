d5f1612a547bf2221c32ffbc722f2153e2c42455
Cuby92 <jakub.bartul@hotmail.com>
2026-03-14 11:38:34 +0100

    Add metadata

--- biogassolutions/src/app/kontakt/layout.tsx
+++ biogassolutions/src/app/kontakt/layout.tsx
@@ -2,8 +2,8 @@ import { Metadata } from 'next';
 import './page.css';
 
 export const metadata: Metadata = {
-    title: '',
-    description: ''
+    title: 'Kontakt i wycena - Biogas Solutions',
+    description: 'Wyślij zapytanie o bezpłatną wycenę instalacji biogazu lub biometanu - działamy w całej Polsce i wspieramy pozyskanie dofinansowania.'
 };
 
 function Layout({ children }: Readonly<{ children: React.ReactNode }>) {
