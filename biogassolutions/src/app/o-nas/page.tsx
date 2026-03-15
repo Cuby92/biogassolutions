f8e54427b4589984599b436e1232b8b2d8d727eb
Cuby92 <jakub.bartul@hotmail.com>
2026-03-15 19:39:38 +0100

    Change styles to s

--- biogassolutions/src/app/o-nas/page.tsx
+++ biogassolutions/src/app/o-nas/page.tsx
@@ -1,11 +1,13 @@
 import Link from 'next/link';
 import styles from './page.module.css';
 
+const s = styles;
+
 function About() {
     return (
         <>
             <div className="content">
-                <div id="AboutSection1" className={`cover ${styles.section1}`}>
+                <div id="AboutSection1" className={`cover ${s.section1}`}>
                     <div className="content">
                         <h1 className="hero-header">Kim jesteśmy?</h1>
                         <p className="lead">Specjalizujemy się w projektowaniu, budowie i uruchamianiu instalacji biogazu i biometanu dla sektora rolnictwa i przemysłu - od małych biogazowni rolniczych o mocy kilkudziesięciu kW po duże instalacje przemysłowe o mocy kilku MW. Celem jest efektywne wykorzystanie odpadów organicznych do produkcji energii i paliw odnawialnych w bezpiecznych i automatycznych instalacjach przetwarzania odpadów.</p>
@@ -14,53 +16,53 @@ function About() {
                 </div>
 
                 <div className="main-content">
-                    <div id="AboutSection2" className={styles.section2}>
+                    <div id="AboutSection2" className={s.section2}>
                         <h2>Jak pracujemy?</h2>
-                        <div className={`card ${styles.card}`}>
-                            <img className={styles.icon} src="/img/icons/hand.png" alt="Ikona finansowania" />
+                        <div className={`card ${s.card}`}>
+                            <img className={s.icon} src="/img/icons/hand.png" alt="Ikona finansowania" />
                             <p>Wspieramy pozyskanie dofinansowania, przygotowując dokumentację i rozliczenia.</p>
                         </div>
-                        <div className={`card ${styles.card}`}>
-                            <img className={styles.icon} src="/img/icons/workflow.png" alt="Ikona przepływu pracy" />
+                        <div className={`card ${s.card}`}>
+                            <img className={s.icon} src="/img/icons/workflow.png" alt="Ikona przepływu pracy" />
                             <p>Kompleksowo prowadzimy inwestycję: studium wykonalności, projekt, decyzje, budowa, rozruch i wsparcie eksploatacyjne.</p>
                         </div>
-                        <div className={`cta ${styles.cta}`}><Link className={`cta-button ${styles['cta-button']}`} href="/kontakt">Uzyskaj bezpłatną wstępną wycenę</Link></div>
+                        <div className={`cta ${s.cta}`}><Link className={`cta-button ${styles['cta-button']}`} href="/kontakt">Uzyskaj bezpłatną wstępną wycenę</Link></div>
                     </div>
 
-                    <div id="AboutSection3" className={styles.section3}>
+                    <div id="AboutSection3" className={s.section3}>
                         <h2>Technologia i dobór</h2>
 
-                        <div className={`card ${styles.card}`}>
-                            <img className={styles.icon} src="/img/icons/settings.png" alt="Ikona kół zębatych" />
+                        <div className={`card ${s.card}`}>
+                            <img className={s.icon} src="/img/icons/settings.png" alt="Ikona kół zębatych" />
                             <p>Konfiguracje dobieramy do dostępnych substratów, sezonowości i profilu zapotrzebowania na energię.</p>
                         </div>
 
-                        <div className={`card ${styles.card}`}>
-                            <img className={styles.icon} src="/img/icons/elasticity.png" alt="Ikona elastyczności" />
+                        <div className={`card ${s.card}`}>
+                            <img className={s.icon} src="/img/icons/elasticity.png" alt="Ikona elastyczności" />
                             <p>Zapewniamy elastyczność instalacji dzięki zastosowaniu buforów technologicznych, co pozwala dostosować się do zmienności rynku substratów i utrzymać wysoką niezawodność na lata.</p>
                         </div>
 
-                        <div className={`card ${styles.card}`}>
-                            <img className={styles.icon} src="/img/icons/industry-tank.png" alt="Ikona zbiornika" />
+                        <div className={`card ${s.card}`}>
+                            <img className={s.icon} src="/img/icons/industry-tank.png" alt="Ikona zbiornika" />
                             <p>Stawiamy na materiały najwyższej jakości.</p>
                         </div>
-                        <div className={`cta ${styles.cta}`}><Link className="cta-button" href="/technologia">Poznaj technologię</Link></div>
+                        <div className={`cta ${s.cta}`}><Link className="cta-button" href="/technologia">Poznaj technologię</Link></div>
                     </div>
 
-                    <div id="AboutSection4" className={styles.section4}>
+                    <div id="AboutSection4" className={s.section4}>
                         <h2>Gdzie działamy?</h2>
 
-                        <div className={`card ${styles.card}`}>
-                            <img className={styles.icon} src="/img/icons/planet-earth.png" alt="Ikona Ziemi" />
+                        <div className={`card ${s.card}`}>
+                            <img className={s.icon} src="/img/icons/planet-earth.png" alt="Ikona Ziemi" />
                             <p>Działamy w całej Polsce oraz w krajach europejskich; oferujemy konsultacje na miejscu.</p>
                         </div>
 
-                        <div className={`card ${styles.card}`}>
-                            <img className={styles.icon} src="/img/icons/graduate-hat.png" alt="Ikona czapki absolwenta" />
+                        <div className={`card ${s.card}`}>
+                            <img className={s.icon} src="/img/icons/graduate-hat.png" alt="Ikona czapki absolwenta" />
                             <p>Zapewniamy szkolenia, zdalny monitoring 24/7 i serwis zgodnie z zapisami umowy serwisowej (SLA).</p>
                         </div>
 
-                        <div className={`cta ${styles.cta}`}><Link className={`cta-button ${styles['cta-button']}`} href="/kontakt">Umów się na bezpłatną konsultację</Link></div>
+                        <div className={`cta ${s.cta}`}><Link className={`cta-button ${styles['cta-button']}`} href="/kontakt">Umów się na bezpłatną konsultację</Link></div>
                     </div>
                 </div>
             </div>
--- biogassolutions/src/app/page.tsx
+++ biogassolutions/src/app/page.tsx
@@ -2,10 +2,12 @@ import Image from "next/image";
 import styles from './page.module.css';
 import Link from "next/link";
 
+const s = styles;
+
 function Home() {
     return (
         <section className="content">
-            <section className={`cover ${styles.section1}`} id="HomeSection1">
+            <section className={`cover ${s.section1}`} id="HomeSection1">
                 <div className="content">
                     <h1 className="hero-header">Tańsza energia i ciepło z własnych odpadów - instalacje biogazu</h1>
                     <p className="lead">Wykorzystujemy lokalne odpady organiczne, aby bezpiecznie wytwarzać prąd i ciepło w kogeneracji - od koncepcji i pozwoleń po rozruch i serwis. Zapewniamy wsparcie w dofinansowaniu oraz zdalny monitoring 24/7 i szybką reakcję serwisu zgodnie z umową.</p>
@@ -14,66 +16,66 @@ function Home() {
             </section>
 
             <section className="main-content">
-                <section className={styles.section2} id="HomeSection2">
+                <section className={s.section2} id="HomeSection2">
                     <h2>Co robimy?</h2>
-                    <div className={`card ${styles.column} ${styles.card}`}>
-                        <img className={styles.icon} src="/img/icons/biogas.png" alt="Ikona biogazowni" />
-                        <p className={styles.p}>Mikrobiogazownie do 50 kW</p>
+                    <div className={`card ${s.column} ${s.card}`}>
+                        <img className={s.icon} src="/img/icons/biogas.png" alt="Ikona biogazowni" />
+                        <p className={s.p}>Mikrobiogazownie do 50 kW</p>
                     </div>
-                    <div className={`card ${styles.column} ${styles.card}`}>
-                        <img className={styles.icon} src="/img/icons/power-plant.png" alt="Ikona dużej biogazowni" />
-                        <p className={styles.p}>Biogazownie 50-499 kW i {'>'}500 kW</p> 
+                    <div className={`card ${s.column} ${s.card}`}>
+                        <img className={s.icon} src="/img/icons/power-plant.png" alt="Ikona dużej biogazowni" />
+                        <p className={s.p}>Biogazownie 50-499 kW i {'>'}500 kW</p> 
                     </div>
-                    <div className={`card ${styles.column} ${styles.card}`}    >
-                        <img className={styles.icon} src="/img/icons/methane.png" alt="Ikona metanu" />
-                        <p className={styles.p}>Biometanownie</p>  
+                    <div className={`card ${s.column} ${s.card}`}    >
+                        <img className={s.icon} src="/img/icons/methane.png" alt="Ikona metanu" />
+                        <p className={s.p}>Biometanownie</p>  
                     </div>
-                    <div className={`cta ${styles.cta}`}>
+                    <div className={`cta ${s.cta}`}>
                         <Link href="/oferta" className={`cta-button ${styles['cta-button']}`}>Poznaj naszą ofertę</Link>
                     </div>
                 </section>
 
-                <section className={styles.section3} id="HomeSection3">
+                <section className={s.section3} id="HomeSection3">
                     <h2>Dlaczego my?</h2>
                     <div className={`grid-wrapper ${styles['grid-wrapper']}`}>
-                        <div className={`card ${styles.card}`}>
-                            <img className={styles.icon} src="/img/icons/workflow.png" alt="Ikona przepływu pracy" />
+                        <div className={`card ${s.card}`}>
+                            <img className={s.icon} src="/img/icons/workflow.png" alt="Ikona przepływu pracy" />
                             <p>Kompleksowa obsługa "od A do Z" (studium, projekt, decyzje, budowa, rozruch, serwis) i wsparcie pozyskania dotacji.</p>
                         </div>
-                        <div className={`card ${styles.card}`}>
-                            <img className={styles.icon} src="/img/icons/technology.png" alt="Ikona technologii" />
+                        <div className={`card ${s.card}`}>
+                            <img className={s.icon} src="/img/icons/technology.png" alt="Ikona technologii" />
                             <p>Zaawansowane systemy procesowe - w zakresie urządzeń i elementów procesowych (w tym procesów higienizacji oraz sterylizacji), zdalnego sterowania oraz instalacji dla funkcjonalności obiektów.</p>
                         </div>
-                        <div className={`card ${styles.card}`}>
-                            <img className={styles.icon} src="/img/icons/24-hours-service.png" alt="Ikona obsługa 24/7" />
+                        <div className={`card ${s.card}`}>
+                            <img className={s.icon} src="/img/icons/24-hours-service.png" alt="Ikona obsługa 24/7" />
                             <p>Zdalny monitoring 24/7, szkolenia operatorów i standard reakcji serwisu do 48 h z opcją skrócenia wg umowy.</p>
                         </div>
-                        <div className={`card ${styles.card}`}>
-                            <img className={styles.icon} src="/img/icons/shield.png" alt="Ikona tarczy" />
+                        <div className={`card ${s.card}`}>
+                            <img className={s.icon} src="/img/icons/shield.png" alt="Ikona tarczy" />
                             <p>Bezpieczeństwo i niezawodność instalacji w zakresie całości obiektów oraz poszczególnych urządzeń i elementów.</p>
                         </div>
                     </div>
-                    <div className={`cta ${styles.cta}`}>
+                    <div className={`cta ${s.cta}`}>
                         <Link className={`cta-button ${styles['cta-button']}`} href="/o-nas">Dowiedz się więcej</Link>
                     </div>
                 </section>
 
-                <section className={styles.section4} id="HomeSection4">
+                <section className={s.section4} id="HomeSection4">
                     <h2>Proces inwestycyjny</h2>
-                    <img className={styles.timeline} src="/img/timeline.png" alt="Przebieg procesu inwestycyjnego - oś czasu" />
+                    <img className={s.timeline} src="/img/timeline.png" alt="Przebieg procesu inwestycyjnego - oś czasu" />
                 </section>
 
-                <section className={styles.section5} id="HomeSection5">
+                <section className={s.section5} id="HomeSection5">
                     <h2>Finansowanie</h2>
                     <p>Przygotowujemy dokumentację i wnioski oraz wspieramy rozliczenia w programach krajowych i unijnych.</p>
 
-                    <div className={styles.grants}>
-                        <img className={styles.grantLogo} src="/img/grants/feng-logo.png"            alt="Fundusze Europejskie dla Nowoczesnej Gospodarki"/>
-                        <img className={styles.grantLogo} src="/img/grants/nfosigw-logo.png"         alt="Narodowy Fundusz Ochrony Środowiska i Gospodarki Wodnej" />
-                        <img className={styles.grantLogo} src="/img/grants/energia-dla-wsi-logo.png" alt="Energia Dla Wsi" />
+                    <div className={s.grants}>
+                        <img className={s.grantLogo} src="/img/grants/feng-logo.png"            alt="Fundusze Europejskie dla Nowoczesnej Gospodarki"/>
+                        <img className={s.grantLogo} src="/img/grants/nfosigw-logo.png"         alt="Narodowy Fundusz Ochrony Środowiska i Gospodarki Wodnej" />
+                        <img className={s.grantLogo} src="/img/grants/energia-dla-wsi-logo.png" alt="Energia Dla Wsi" />
                     </div>
 
-                    <div className={`cta ${styles.cta}`}>
+                    <div className={`cta ${s.cta}`}>
                         <Link className={`cta-button ${styles['cta-button']}`} href="/kontakt">Uzyskaj darmową wstępną wycenę</Link>
                     </div>
                 </section>
