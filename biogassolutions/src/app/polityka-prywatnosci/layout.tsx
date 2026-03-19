49c49d230b7ab583091e8f4a5ed7a570ca0795ef
Cuby92 <jakub.bartul@hotmail.com>
2026-03-19 13:43:10 +0100

    Add metadata

--- biogassolutions/src/app/polityka-prywatnosci/layout.tsx
+++ biogassolutions/src/app/polityka-prywatnosci/layout.tsx
@@ -1,8 +1,8 @@
 import { Metadata } from 'next';
 
 export const metadata: Metadata = {
-    title: '',
-    description: ''
+    title: 'Polityka prywatności - Biogas Solutions',
+    description: 'Polityka Prywatności Biogas Solutions. Sprawdź, w jaki sposób przetwarzamy dane osobowe i wykorzystujemy pliki cookies (ciasteczka) zgodnie z wymogami RODO.'
 };
 
 function Layout({ children }: Readonly<{ children: React.ReactNode }>) {
