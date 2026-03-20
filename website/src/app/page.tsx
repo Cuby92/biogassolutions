017de46cababf9961c3a2fe117d1bbc5c781c515
Cuby92 <jakub.bartul@hotmail.com>
2026-03-20 15:05:53 +0100

    Optimize Home page background with next/image

--- website/src/app/page.tsx
+++ website/src/app/page.tsx
@@ -7,6 +7,15 @@ const s = styles;
 function Home() {
     return (
         <section className="content">
+            <Image
+                src='/img/bg/bg1.jpg'
+                width={3550}
+                height={1895}
+                priority={true}
+                quality={80}
+                alt=''
+                className="pageBg"
+            />
             <section className={`cover ${s.Section1}`} id="HomeSection1">
                 <div className="content">
                     <h1 className="hero-header">Tańsza energia i ciepło z własnych odpadów - instalacje biogazu</h1>
