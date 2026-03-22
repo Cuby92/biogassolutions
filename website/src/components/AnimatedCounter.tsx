a94ce1a2536fcde6ca44919b5f75e0fdb07f0169
Cuby92 <jakub.bartul@hotmail.com>
2026-03-22 11:37:30 +0100

    Add before and after

--- website/src/components/AnimatedCounter.tsx
+++ website/src/components/AnimatedCounter.tsx
@@ -3,14 +3,23 @@
 import { useCounter } from '@/utils/counter';
 import { useInView } from 'react-intersection-observer';
 
+interface Text {
+    text: string;
+    font?: string;
+    fontWeight?: number;
+    color?: string;
+};
+
 interface AnimatedCounterProps {
     end: number;
     duration?: number;
-    decimalPlaces?: number
-    className?: string
+    decimalPlaces?: number;
+    className?: string;
+    before?: Text;
+    after?: Text;
 };
 
-function AnimatedCounter({ end, duration, decimalPlaces, className }: AnimatedCounterProps) {
+function AnimatedCounter({ end, duration, decimalPlaces, className, before, after }: AnimatedCounterProps) {
     const [ref, inView] = useInView({
         triggerOnce: true,
         threshold: 0.3
@@ -23,7 +32,19 @@ function AnimatedCounter({ end, duration, decimalPlaces, className }: AnimatedCo
     });
 
     return (
-        <div ref={ref} className={className || ''}>{ counter }</div>
+        <div ref={ref} className={className || ''}>
+
+            {before && <span style={{fontFamily: before.font || 'var(--poppins)', fontWeight: before.fontWeight || 100, color: before.color || 'inherit'}}>
+                { before.text }
+            </span>}
+
+            { counter }
+
+            {after && <span style={{fontFamily: after.font || 'var(--poppins', fontWeight: after.fontWeight || 100, color: after.color || 'inherit'}}>
+                { after.text }
+            </span>}
+            
+        </div>
     );
 }
 
