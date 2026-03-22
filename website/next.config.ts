3941e80df8462fc85e7085ddde8d022023f7fb2d
Cuby92 <jakub.bartul@hotmail.com>
2026-03-22 11:51:05 +0100

    Fix bugs

--- website/next.config.ts
+++ website/next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   reactCompiler: true,
   images: {
-    qualities: [50, 75, 80, 90, 100]
+    qualities: [50, 60, 80, 90, 100]
   }
 };
 
