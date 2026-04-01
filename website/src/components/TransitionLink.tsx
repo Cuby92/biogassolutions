44f85a71c33cd9e339520f4a084ccc1ce493f25a
Cuby92 <jakub.bartul@hotmail.com>
2026-04-01 20:24:14 +0200

    Update TransitionLink.tsx

--- website/src/components/TransitionLink.tsx
+++ website/src/components/TransitionLink.tsx
@@ -1,14 +1,25 @@
 'use client';
 
 import Link, { LinkProps } from 'next/link';
+import { useRouter } from 'next/navigation';
 
 interface Props extends LinkProps {
     children: React.ReactNode;
+    href: string;
 }
 
 function TransitionLink({ href, children, ...props }: Props) {
+    const router = useRouter();
+
     return (
-        <Link href={href} {...props}>
+        <Link 
+            href={href}
+            {...props}
+            onClick={e => {
+                e.preventDefault();
+                router.push(href);
+            }}
+        >
             { children }
         </Link>
     );
